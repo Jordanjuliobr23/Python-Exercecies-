085a0365b5cc725a236093a181ca93996e2b37d9
Jordan Júlio <164351071+Jordanjuliobr23@users.noreply.github.com>
2024-03-30 20:47:35 -0300

    Add files via upload

---
+++ Fatorial de um número.py
@@ -0,0 +1,11 @@
+print('=-'*100)
+print('Fatorial de um número')
+n=int(input('Digite um número para calcula o seu fatorial: ')) 
+r=1
+c=1
+while c <= n:
+    r= n * c 
+    c= c + 1
+print(f'O fatorial do número {n}, será {r}')
+
+
