12f570b6d058d7b0ebd85a3ce85c76a1f0f5ee12
Jordan Júlio <164351071+Jordanjuliobr23@users.noreply.github.com>
2024-04-01 19:01:07 -0300

    Add files via upload

--- Identificação de pares e ímpares em um conjunto de 10 números!.py
+++ Identificação de pares e ímpares em um conjunto de 10 números!.py
@@ -1,5 +1,8 @@
 print('Trabalhando com um conjunto de 10 números!')
+x = 0
 c= 0
+qtdi=0
+qtdp=0
 soma1=0
 soma2=0 
 while c < 10: 
@@ -7,15 +10,18 @@ while c < 10:
     if n % 2 == 0:
         par = True
         impar = False
-        c= c + 1
+        qtdp= qtdp + 1
     if n % 2 != 0: 
         impar = True
         par = False 
-        c= c + 1
+        qtdi= qtdi + 1 
+    c= c + 1
     if par:
         soma1= soma1 + n
     if impar: 
         soma2= soma2 + n 
-print(f'Existem {soma1} números PARES')
-print(f'Existem {soma2} números IMPARES')
+print(f'A soma dos PARES corresponde a: {soma1} ')
+print(f'A soma dos IMPARES corresponde a: {soma2} ')
+print(f'Existem {qtdp} números PARES')
+print(f'Existem {qtdi} números IMPARES')
 
