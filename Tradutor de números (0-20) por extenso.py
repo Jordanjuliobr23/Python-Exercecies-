5693eaf0c04b88b580567b5895aaa9a501454c41
Jordan Júlio <164351071+Jordanjuliobr23@users.noreply.github.com>
2024-06-05 21:53:47 -0300

    Add files via upload

---
+++ Tradutor de números (0-20) por extenso.py
@@ -0,0 +1,70 @@
+print('=-'*100)
+print('Selection a number from 0 to 20')
+numeros=('Zero','Um','Dois','Tres','Quatro','Cinco','Seis','Sete','Oito','Nove','Dez','Onze','Doze','Treze','Quatorze','Quinze','Dezeseis','Dezesete','Dezoito','Dezenove','Vinte' )
+n=int(input('Enter a number from 0 to 20 to see it in full in Portuguese (0-20): '))
+if n == 0:
+    print('A number it in full Portuguese is: ')
+    print(numeros[0])
+elif n == 1:
+    print('A number it in full Portuguese is: ')
+    print(numeros[1])
+elif n == 2:
+    print('A number it in full Portuguese is: ')
+    print(numeros[2])
+elif n == 3:
+    print('A number it in full Portuguese is: ')
+    print(numeros[3])
+elif n == 4:
+    print('A number it in full Portuguese is: ')
+    print(numeros[4])
+elif n == 5:
+    print('A number it in full Portuguese is: ')
+    print(numeros[5])
+elif n == 6:
+    print('A number it in full Portuguese is: ')
+    print(numeros[6])
+elif n == 7:
+    print('A number it in full Portuguese is: ')
+    print(numeros[7]) 
+elif n == 8:
+    print('A number it in full Portuguese is: ')
+    print(numeros[8])
+elif n == 9:
+    print('A number it in full Portuguese is: ')
+    print(numeros[9])
+elif n == 10:
+    print('A number it in full Portuguese is: ')
+    print(numeros[10])
+elif n == 11:
+    print('A number it in full Portugueses is: ')
+    print(numeros[11])
+elif n == 12:
+    print('A number it in full Portuguese is: ')
+    print(numeros[12])
+elif n == 13:
+    print('A number it in full Portuguese is: ')
+    print(numeros[13])
+elif n == 14:
+    print('A number it in full Portuguese is: ')
+    print(numeros[14])
+elif n == 15:
+    print('A number it in full Portuguese is: ')
+    print(numeros[15])
+elif n == 16:
+    print('A number it in full Portuguese is: ')
+    print(numeros[16])
+elif n == 17:
+    print('A number it in full Portuguese is: ')
+    print(numeros[17])
+elif n == 18: 
+    print('A number it in full Portuguese is: ')
+    print(numeros[18])
+elif n == 19:
+    print('A number it in full Portuguese is: ')
+    print(numeros[19])
+elif n == 20:
+    print('A number it in full Portuguese is: ')
+    print(numeros[20])
+else: 
+    print("This number does'nt follow the range 0-20!")
+    print('PROGRAM CLOSED')
